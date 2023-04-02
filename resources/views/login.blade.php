40f517f33edc605f3cb9b4a579f72b633f0a1092
quentiin19 <quentin.bosseboeuf@gmail.com>
2023-04-02 18:45:12 +0200

    Register + login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<body>
+    <div class="d-inline-flex p-2  rounded position-absolute top-50 start-50 translate-middle ">
+        <form>
+            <h2 class="my-4">Connexion</h2>
+            <div class="form-group">
+                <label class ="pb-2">E-mail</label>
+                <input type="email" class="form-control">
+            </div>
+            <div class="form-group py-2">
+                <label class ="pb-2">Mot de passe</label>
+                <input type="password" class="form-control">
+            </div>
+            <div class="d-grid gap-2 pt-3">
+                <button type="submit" class="btn btn-primary">Suivant</button>
+            </div>
+            <div class="form-group pt-3 text-center">
+                <a href="#" class="link-secondary ">Vous avez des difficultés de connexion ?</a>
+            </div>
+            <div class="py-3">
+                <hr>
+            </div>
+            <div class="d-grid gap-2 ">
+                <button type="submit" class="btn btn-secondary">Créer un compte</button>
+            </div>
+        </form>
+    </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ resources/views/register.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<body>
+    <div class="d-inline-flex p-2  rounded position-absolute top-50 start-50 translate-middle ">
+        <form>
+            <h2 class="my-4">Créer un compte</h2>
+            <div class="form-group">
+                <label class ="pb-2">Prenom</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="form-group py-2">
+                <label class ="pb-2">Nom</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="form-group py-2">
+                <label class ="pb-2">E-mail</label>
+                <input type="email" class="form-control">
+            </div>
+            <div class="form-group py-2">
+                <label class ="pb-2">Mot de passe</label>
+                <input type="password" class="form-control">
+            </div>
+            <div class="form-group py-2">
+                <label class ="pb-2">Confirmation Mot de passe</label>
+                <input type="password" class="form-control">
+            </div>
+            <div class="pt-2">
+                <p>En continuant, je comprends et j'accepte l'<a class ="link-secondary" href="www.google.com">Avis de confidentialité</a> et les <a class ="link-secondary" href="#">Conditions d'utilisation</a> de l'Atelier des Gourmets pour la création d'un compte</p> 
+            </div>
+            <div class="d-grid gap-2 pt-3 ">
+                <button type="submit" class="btn btn-secondary">Créer un compte</button>
+            </div>
+        </form>
+    </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
