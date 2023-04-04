4bc587d6a7835329a571903dbeb07aa9755a813c
jordan95v <jordan.dfrsne@gmail.com>
2023-04-04 18:32:15 +0200

    Added main layout + modified login and register page.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,117 +1,85 @@
-<!DOCTYPE html>
-<html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Home</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    </head>
+@extends('main_layout')
 
-    <body class="pb-4">
-        {{-- Navbar --}}
-        <nav class="navbar navbar-expand-lg bg-light">
-            <div class="container-fluid text-center">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar_content">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+@section('content')
+    {{-- Logo --}}
+    <div class="text-center">
+        <img src="{{ asset('/images/logo.png') }}" alt="logo" class="mt-4 img-fluid w-25">
+    </div>
 
-                <div class="collapse navbar-collapse" id="navbar_content">
-                    <ul class="navbar-nav text-center ms-auto me-auto">
-                        <li class="nav-item">
-                            <h4><a class="nav-link active" aria-current="page" href="#">🏡 Accueil</a></h4>
-                        </li>
+    {{-- Main content --}}
+    <div class="container-fluid p-5">
 
-                        <li class="nav-item">
-                            <h4><a class="nav-link" href="#">🏫 Cours</a></h4>
-                        </li>
-
-                        <li class="nav-item dropdown">
-                            <h4><a class="nav-link" href="#">📚 Leçon</a></h4>
-                        </li>
-
-                        <li class="nav-item">
-                            <h4><a class="nav-link">💸 Boutique</a></h4>
-                        </li>
-                    </ul>
+        {{-- Atelier --}}
+        <div class="row p-5 shadow-lg rounded">
+            <div class="col-lg-4">
+                <div class="text-center">
+                    <img src="{{ asset('/images/atelier.jpg') }}" alt="atelier" class="img-fluid rounded">
                 </div>
             </div>
-        </nav>
+            <div class="col-lg-8">
+                <h1 class="mb-5">🍲 Découvrez nos ateliers</h1>
+                <p>
+                    Nous sommes ravis de vous proposer une <b>expérience gastronomique</b> unique à travers nos ateliers
+                    de cuisine.<br>
+                    Que vous soyez un novice ou un passionné, notre équipe de <b>chefs expérimentés</b> est là pour vous
+                    guider à chaque
+                    étape de la préparation de vos plats préférés.<br><br>
 
-        {{-- Logo --}}
-        <div class="text-center">
-            <img src="{{ asset('/storage/logo.png') }}" alt="logo" class="img-fluid">
-        </div>
+                    Nos ateliers culinaires offrent une <b>opportunité unique</b> de découvrir de nouvelles recettes et
+                    techniques de cuisine.
+                    Vous aurez la chance d'apprendre à cuisiner des plats délicieux, à partir d'ingrédients <b>frais et
+                        locaux</b>, dans une
+                    ambiance conviviale et chaleureuse.<br>
+                    Vous pourrez également échanger avec d'autres passionnés de cuisine, partager des astuces et des
+                    conseils,
+                    et découvrir de <b>nouveaux horizons culinaires</b>.<br><br>
 
-        {{-- Main content --}}
-        <div class="container-fluid p-5">
+                    Nous proposons une variété de thèmes, des plats <b>traditionnels</b> aux recettes plus
+                    <b>exotiques</b>, pour répondre aux goûts et
+                    aux intérêts de chacun.<br><br>
 
-            {{-- Atelier --}}
-            <div class="row p-5 shadow rounded">
-                <div class="col-lg-4">
-                    <div class="text-center">
-                        <img src="{{ asset('/storage/atelier.jpg') }}" alt="atelier" class="img-fluid rounded ">
-                    </div>
-                </div>
-                <div class="col-lg-8">
-                    <h1 class="mb-5">🍲 Découvrez nos ateliers</h1>
-                    <p>
-                        Nous sommes ravis de vous proposer une <b>expérience gastronomique</b> unique à travers nos ateliers de cuisine.<br>
-                        Que vous soyez un novice ou un passionné, notre équipe de <b>chefs expérimentés</b> est là pour vous guider à chaque 
-                        étape de la préparation de vos plats préférés.<br><br>
-    
-                        Nos ateliers culinaires offrent une <b>opportunité unique</b> de découvrir de nouvelles recettes et techniques de cuisine. 
-                        Vous aurez la chance d'apprendre à cuisiner des plats délicieux, à partir d'ingrédients <b>frais et locaux</b>, dans une 
-                        ambiance conviviale et chaleureuse.<br>
-                        Vous pourrez également échanger avec d'autres passionnés de cuisine, partager des astuces et des conseils, 
-                        et découvrir de <b>nouveaux horizons culinaires</b>.<br><br>
-    
-                        Nous proposons une variété de thèmes, des plats <b>traditionnels</b> aux recettes plus <b>exotiques</b>, pour répondre aux goûts et 
-                        aux intérêts de chacun.<br><br>
-                        
-                        Alors, <b>rejoignez-nous</b> pour une expérience culinaire unique et inoubliable ! 
-                        Consultez notre calendrier d'ateliers pour trouver celui qui vous convient le mieux et <b>réservez</b> votre place dès maintenant. 
-                        Nous avons hâte de cuisiner avec vous !
-                    </p>
-                </div>
-            </div>
-
-            {{-- Course --}}
-            <div class="row p-5 shadow rounded mt-4">
-                <div class="col-lg-8">
-                    <h1 class="mb-5">🧆 Découvrez nos cours de cuisine (pour tous les niveaux 🧑🏽‍🍳)</h1>
-                    <p>
-                        Nous sommes heureux de vous proposer également des cours de cuisine pour approfondir vos connaissances 
-                        culinaires et <b>perfectionner</b> votre technique.<br>
-                        Nos cours sont conçus pour les <b>passionnés de cuisine</b> qui souhaitent aller plus loin dans leur apprentissage 
-                        et <b>améliorer</b> leur compétences culinaires.<br><br>
-
-                        Dispensés par des chefs <b>professionnels expérimentés</b>, qui vous guideront à travers des techniques <b>avancées</b>,
-                        des recettes <b>élaborées</b> et des présentations culinaires élégantes.<br><br>
-
-                        Nous proposons des cours sur une variété de thèmes culinaires, tels que la cuisine <b>française</b>, <b>italienne</b>, <b>asiatique</b> et bien plus encore.<br>
-                        Tuut cela dans une ambiance <b>conviviale et agréable</b>, où vous pourrez échanger avec d'autres passionnés de cuisine et 
-                        <b>partager</b> votre expérience culinaire.<br><br>
-
-                        Alors, si vous êtes passionné de cuisine et souhaitez vous <b>perfectionner</b>, consultez notre calendrier 
-                        de cours pour trouver celui qui vous convient le mieux et <b>réservez</b> votre place dès maintenant.<br> 
-                        Nous avons hâte de vous aider à atteindre votre <b>potentiel culinaire maximal</b> !
-                    </p>
-                </div>
-                <div class="col-lg-4">
-                    <div class="text-center">
-                        <img src="{{ asset('storage/course.jpg') }}" alt="atelier" class="img-fluid rounded">
-                    </div>
-                </div>
+                    Alors, <b>rejoignez-nous</b> pour une expérience culinaire unique et inoubliable !
+                    Consultez notre calendrier d'ateliers pour trouver celui qui vous convient le mieux et
+                    <b>réservez</b> votre place dès maintenant.
+                    Nous avons hâte de cuisiner avec vous !
+                </p>
             </div>
         </div>
 
-        <footer class="text-center">
-            <p>L'Atelier des Gourmets ©2023 - <a href="" class="link-secondary">Mentions Légales</a> - <a href="" class="link-secondary">Contact</a></p>
-        </footer>
+        {{-- Course --}}
+        <div class="row p-5 shadow-lg rounded mt-5">
+            <div class="col-lg-8">
+                <h1 class="mb-5">🧆 Découvrez nos cours de cuisine (pour tous les niveaux 🧑🏽‍🍳)</h1>
+                <p>
+                    Nous sommes heureux de vous proposer également des cours de cuisine pour approfondir vos
+                    connaissances
+                    culinaires et <b>perfectionner</b> votre technique.<br>
+                    Nos cours sont conçus pour les <b>passionnés de cuisine</b> qui souhaitent aller plus loin dans leur
+                    apprentissage
+                    et <b>améliorer</b> leur compétences culinaires.<br><br>
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" 
-        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
-        </script>
-    </body>
-</html>
+                    Dispensés par des chefs <b>professionnels expérimentés</b>, qui vous guideront à travers des
+                    techniques <b>avancées</b>,
+                    des recettes <b>élaborées</b> et des présentations culinaires élégantes.<br><br>
+
+                    Nous proposons des cours sur une variété de thèmes culinaires, tels que la cuisine <b>française</b>,
+                    <b>italienne</b>, <b>asiatique</b> et bien plus encore.<br>
+                    Tuut cela dans une ambiance <b>conviviale et agréable</b>, où vous pourrez échanger avec d'autres
+                    passionnés de cuisine et
+                    <b>partager</b> votre expérience culinaire.<br><br>
+
+                    Alors, si vous êtes passionné de cuisine et souhaitez vous <b>perfectionner</b>, consultez notre
+                    calendrier
+                    de cours pour trouver celui qui vous convient le mieux et <b>réservez</b> votre place dès
+                    maintenant.<br>
+                    Nous avons hâte de vous aider à atteindre votre <b>potentiel culinaire maximal</b> !
+                </p>
+            </div>
+            <div class="col-lg-4">
+                <div class="text-center">
+                    <img src="{{ asset('images/course.jpg') }}" alt="atelier" class="img-fluid rounded">
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,44 +1,51 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-</head>
-<body>
-    <div class="d-inline-flex p-2  rounded position-absolute top-50 start-50 translate-middle ">
-        <form>
+@extends('main_layout')
+
+@section('content')
+    <div class="rounded d-flex my-5 py-5 justify-content-center">
+        <form method="POST" class="p-5 rounded shadow-lg">
             <h2 class="my-4">Créer un compte</h2>
-            <div class="form-group">
-                <label class ="pb-2">Prenom</label>
-                <input type="text" class="form-control">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="form-group">
+                        <label class="pb-2">Prenom</label>
+                        <input type="text" class="form-control">
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="form-group">
+                        <label class="pb-2">Nom</label>
+                        <input type="text" class="form-control">
+                    </div>
+                </div>
             </div>
             <div class="form-group py-2">
-                <label class ="pb-2">Nom</label>
-                <input type="text" class="form-control">
-            </div>
-            <div class="form-group py-2">
-                <label class ="pb-2">E-mail</label>
+                <label class="pb-2">E-mail</label>
                 <input type="email" class="form-control">
             </div>
-            <div class="form-group py-2">
-                <label class ="pb-2">Mot de passe</label>
-                <input type="password" class="form-control">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="form-group py-2">
+                        <label class="pb-2">Mot de passe</label>
+                        <input type="password" class="form-control">
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="form-group py-2">
+                        <label class="pb-2">Confirmation du mot de passe</label>
+                        <input type="password" class="form-control">
+                    </div>
+                </div>
             </div>
-            <div class="form-group py-2">
-                <label class ="pb-2">Confirmation Mot de passe</label>
-                <input type="password" class="form-control">
-            </div>
+
+            {{-- CGU --}}
             <div class="pt-2">
-                <p>En continuant, je comprends et j'accepte l'<a class ="link-secondary" href="www.google.com">Avis de confidentialité</a> et les <a class ="link-secondary" href="#">Conditions d'utilisation</a> de l'Atelier des Gourmets pour la création d'un compte</p> 
+                <p>En continuant, je comprends et j'accepte l'<a class="link-secondary" href="www.google.com">Avis de
+                        confidentialité</a> et les <a class="link-secondary" href="#">Conditions d'utilisation</a> de
+                    l'Atelier des Gourmets pour la création d'un compte</p>
             </div>
             <div class="d-grid gap-2 pt-3 ">
-                <button type="submit" class="btn btn-secondary">Créer un compte</button>
+                <button type="submit" class="btn btn-success">Créer un compte</button>
             </div>
         </form>
     </div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-</body>
-</html>
+@endsection
