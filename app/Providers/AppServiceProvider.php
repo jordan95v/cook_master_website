6df9d48969469575ff69e4925b8a503c68c0c38a
jordan95v <jordan.dfrsne@gmail.com>
2023-05-11 07:54:10 +0200

    Added env for deployment.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,5 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (env("DEPLOY_WITH_CADDY")) {
+            URL::forceScheme('https');
+        }
     }
 }
