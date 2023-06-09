d21c4e57d9dd0909ac1c51d55d5cd103b7174eff
jordan95v <jordan.dfrsne@gmail.com>
2023-06-09 16:09:00 +0200

    Modified front for basket.

--- resources/views/components/shop/basket-total.blade.php
+++ resources/views/components/shop/basket-total.blade.php
@@ -4,17 +4,24 @@
         $totalPrice += $item->product->price * $item->quantity;
     }
     $priceWithShippment = $totalPrice + 3;
+    $discount = $priceWithShippment * 0.05;
 @endphp
 <p class="text-end font-mono mb-2">
     {{ __('Total') }}: {{ $priceWithShippment }}€ ({{ $totalPrice }}€ + {{ 3 }}€ {{ __('Shippment') }})
 </p>
 
-@if (Auth::user()->total_discount)
+@if (Auth::user()->isSubscribed() || Auth::user()->total_discount)
     <p class="text-end font-mono mb-2">
-        {{ __('Total discount') }}: {{ Auth::user()->total_discount }}€
+        {{ __('Total discount') }}:
+        @if (Auth::user()->total_discount)
+            {{ Auth::user()->total_discount }}€ {{ __('(your wallet)') }}
+        @endif
+        @if (Auth::user()->isSubscribed())
+            {{ $discount }}€ {{ __('(subscription)') }}
+        @endif
     </p>
 
     <p class="text-end font-mono mb-2">
-        {{ __('Price with discount') }}: {{ $priceWithShippment - Auth::user()->total_discount }}€
+        {{ __('Price with discount') }}: {{ $priceWithShippment - Auth::user()->total_discount - $discount }}€
     </p>
 @endif
