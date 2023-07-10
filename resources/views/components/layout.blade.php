de2d591e0a8b0da4b9af2a017078bab3a7394cbe
jordan95v <jordan.dfrsne@gmail.com>
2023-07-10 21:15:40 +0200

    Google AdSense

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -32,6 +32,9 @@
             <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5629070617397142"
                 crossorigin="anonymous"></script>
         @endif
+    @else
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5629070617397142"
+            crossorigin="anonymous"></script>
     @endauth
 </head>
 
